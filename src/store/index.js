afc5980681c6400197e6c8d5fe5bead52f262b74
GregPinheiro <gpinheirojunior@gmail.com>
2021-10-24 18:04:39 -0300

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import { createStore } from "redux";
+
+const INITIAL_STATE = {
+  activeLesson: {},
+  activeModule: {},
+  modules: [
+    {
+      id: 1,
+      title: "Iniciando com react",
+      lessons: [
+        { id: 1, title: "Primeira aula" },
+        { id: 2, title: "Segunda aula" },
+        { id: 3, title: "Terceira aula" },
+      ],
+    },
+    {
+      id: 2,
+      title: "Avancado de react",
+      lessons: [
+        { id: 1, title: "Contest API" },
+        { id: 2, title: "REDUX" },
+        { id: 3, title: "REDUX SAGA" },
+      ],
+    },
+  ],
+};
+
+function reducer(state = INITIAL_STATE, action) {
+  if (action.type === "TOGGLE_LESSON") {
+    return {
+      ...state,
+      activeModule: action.module,
+      activeLesson: action.lesson,
+    };
+  }
+  return state;
+}
+
+const store = createStore(reducer);
+
+export default store;
